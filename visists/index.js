71152101cd58f7be2949fb17d7d572d5683fe254
prajan <prajan.shrestha@gmail.com>
2021-12-16 11:27:21 +0100

    Running version of Visists app. simpleweb. Also adding commands section

--- visists/index.js
+++ visists/index.js
@@ -10,12 +10,12 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
+    res.send('Prajan - Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
 
-app.listen(4001, () => {
-  console.log('Listening on port 4001');
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
 });
 
